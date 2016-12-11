715a9f7708bd61011e902ee3ee75a8700553577f
Michael Gord <michael.l.gord@gmail.com>
2016-12-11 15:31:03 -0500

    [bug] fix spelling

--- js/price.js
+++ js/price.js
@@ -4,7 +4,7 @@ xbtc.onreadystatechange = function () {
 	if (xbtc.readyState == 4) {
 		var ticker = JSON.parse(xbtc.responseText);
 		var price = ticker.last;
-		cosument.getElementById('btc').innerHTML = "$" + price;
+		document.getElementById('btc').innerHTML = "$" + price;
 	}	
 };
 
