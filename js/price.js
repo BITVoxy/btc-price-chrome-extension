fd1b6ba3c68e251035e3f0b889723e62d595c623
Lance Cameron <vishtany@gmail.com>
2017-10-29 19:28:51 -0500

    Update price.js

--- js/price.js
+++ js/price.js
@@ -1,11 +1,13 @@
-var xbtc = new XMLHttpRequest();
-xbtc.open('GET', 'https://api.bitcoinaverage.com/ticker/global/USD/', true);
-xbtc.onreadystatechange = function () {
-	if (xbtc.readyState == 4) {
-		var ticker = JSON.parse(xbtc.responseText);
-		var price = ticker.last;
-		document.getElementById('btc').innerHTML = "$" + price;
-	}	
+var btcg = new XMLHttpRequest();
+btcg.open('GET', 'https://api.coinmarketcap.com/v1/ticker/bitcoin-gold/', true);
+btcg.onreadystatechange = function() {
+  if (btcg.readyState == 4) {
+    var ticker = JSON.parse(btcg.responseText);
+    var btcgp = ticker[0];
+    var price = btcgp.price_usd;
+    document.getElementById('btg-title').innerHTML = btcgp.name  + " Price";
+    document.getElementById('btg').innerHTML = "$" + price;
+  }
 };
 
-xbtc.send();
+btcg.send();
